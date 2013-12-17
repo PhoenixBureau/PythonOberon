c936175fff1e53fe8e4c72727fd0a4c695b31914
Simon Forman <forman.simon@gmail.com>
2013-12-17 10:17:12 -0800

    Some of the scanner.

--- translated_compiler/ORSX.py
+++ translated_compiler/ORSX.py
@@ -33,23 +33,76 @@ array = 60; record = 61; pointer = 62; const = 63; type_ = 64;
 var = 65; procedure = 66; begin = 67; import_ = 68; module = 69;
 eof = 70;
 
+KWX = {}
+KWX[if_] = "IF"
+KWX[do] = "DO"
+KWX[of] = "OF"
+KWX[or_] = "OR"
+KWX[to] = "TO"
+KWX[in_] = "IN"
+KWX[is_] = "IS"
+KWX[by] = "BY"
+KWX[end] = "END"
+KWX[nil] = "NIL"
+KWX[var] = "VAR"
+KWX[div] = "DIV"
+KWX[mod] = "MOD"
+KWX[for_] = "FOR"
+KWX[else_] = "ELSE"
+KWX[then] = "THEN"
+KWX[true] = "TRUE"
+KWX[type_] = "TYPE"
+KWX[case] = "CASE"
+KWX[elsif] = "ELSIF"
+KWX[false] = "FALSE"
+KWX[array] = "ARRAY"
+KWX[begin] = "BEGIN"
+KWX[const] = "CONST"
+KWX[until] = "UNTIL"
+KWX[while_] = "WHILE"
+KWX[record] = "RECORD"
+KWX[repeat] = "REPEAT"
+KWX[return_] = "RETURN"
+KWX[import_] = "IMPORT"
+KWX[module] = "MODULE"
+KWX[pointer] = "POINTER"
+KWX[procedure] = "PROCEDURE"
+KWX = dict((v, k) for k, v in KWX.iteritems())
+
 '''
   TYPE Ident* = ARRAY IdLen OF CHAR;
 
   VAR ival*, slen*: LONGINT;  (*results of Get*)
     rval*: REAL;
 '''
-#    id*: Ident;  (*for identifiers*)
-id_ = None
 
-R = 'reader'
+slen = 0
+
+id_ = []
+
+R_eot = False
+R = open('ORSX.Mod.txt').read()
+_R = iter(R)
+_pos = 0
+def TextsRead(r):
+  global _pos
+  try:
+    _pos += 1
+    return next(_R)
+  except StopIteration:
+    global R_eot
+    R_eot = True
+    return ''
+
 errpos = 0
 errcnt = 0
 
+ch = ''
+
+str_  = [None] * stringBufSize
+
 
 '''
-    str*: ARRAY stringBufSize OF CHAR;
-
     ch: CHAR;  (*last character read*)
     R: Texts.Reader;
     W: Texts.Writer;
@@ -72,7 +125,7 @@ def CopyId(ident):
 ##  END Pos;
 
 def Pos():
-  return 0
+  return _pos - 1
 
 ##  PROCEDURE Mark*(msg: ARRAY OF CHAR);
 ##    VAR p: LONGINT;
@@ -85,6 +138,7 @@ def Pos():
 ##  END Mark;
 
 def Mark(msg):
+  global errpos, errcnt
   p = Pos()
   if p > errpos and errcnt < 25:
     print "pos", p, msg
@@ -96,7 +150,7 @@ def Mark(msg):
 ##  BEGIN i := 0;
 ##    REPEAT
 ##      IF i < IdLen-1 THEN id[i] := ch; INC(i) END ;
-##      Texts.Read(R, ch)
+##      ch = TextsRead(R)
 ##    UNTIL (ch < "0") OR (ch > "9") & (ch < "A") OR (ch > "Z") & (ch < "a") OR (ch > "z");
 ##    id[i] := 0X; 
 ##    IF i < 10 THEN k := KWX[i-1];  (*search for keyword*)
@@ -106,38 +160,104 @@ def Mark(msg):
 ##    END
 ##  END Identifier;
 
-  PROCEDURE String;
-    VAR i: INTEGER;
-  BEGIN i := 0; Texts.Read(R, ch);
-    WHILE ~R.eot & (ch # 22X) DO
-      IF ch >= " " THEN
-        IF i < stringBufSize-1 THEN str[i] := ch; INC(i) ELSE Mark("string too long") END ;
-      END ;
-      Texts.Read(R, ch)
-    END ;
-    str[i] := 0X; INC(i); Texts.Read(R, ch); slen := i
-  END String;
+def Identifier(sym=None):
+  global ch
+  del id_[:]
+  i = 0
+  while True:
+    if i < IdLen - 1:
+      id_.append(ch)
+      i += 1
+    ch = TextsRead(R)
+    if not ch.isalnum():
+      break
+  if i < 10:
+    try:
+      sym = KWX[''.join(id_)]
+    except KeyError:
+      sym = ident
+  else:
+    sym = ident
+  return sym
 
-  PROCEDURE HexString;
-    VAR i, m, n: INTEGER;
-  BEGIN i := 0; Texts.Read(R, ch);
-    WHILE ~R.eot & (ch # "$") DO
-      WHILE (ch = " ") OR (ch = 9X) OR (ch = 0DX) DO Texts.Read(R, ch) END ;  (*skip*)
-      IF ("0" <= ch) & (ch <= "9") THEN m := ORD(ch) - 30H
-      ELSIF ("A" <= ch) & (ch <= "F") THEN m := ORD(ch) - 37H
-      ELSE m := 0; Mark("hexdig expected")
-      END ;
-      Texts.Read(R, ch);
-      IF ("0" <= ch) & (ch <= "9") THEN n := ORD(ch) - 30H
-      ELSIF ("A" <= ch) & (ch <= "F") THEN n := ORD(ch) - 37H
-      ELSE n := 0; Mark("hexdig expected")
-      END ;
-      IF i < stringBufSize THEN str[i] := CHR(m*10H + n); INC(i) ELSE Mark("string too long") END ;
-      Texts.Read(R, ch)
-    END ;
-    Texts.Read(R, ch); slen := i  (*no 0X appended!*)
-  END HexString;
 
+##  PROCEDURE String;
+##    VAR i: INTEGER;
+##  BEGIN i := 0; ch = TextsRead(R);
+##    WHILE ~R.eot & (ch # 22X) DO
+##      IF ch >= " " THEN
+##        IF i < stringBufSize-1 THEN str[i] := ch; INC(i) ELSE Mark("string too long") END ;
+##      END ;
+##      ch = TextsRead(R)
+##    END ;
+##    str[i] := 0X; INC(i); ch = TextsRead(R); slen := i
+##  END String;
+
+
+def String():
+  global slen, ch
+  i = 0
+  ch = TextsRead(R)
+  while not R_eot and (ch != 0x22):
+    if ch >= " ":
+      if i < stringBufSize-1:
+        str_[i] = ch
+        i += 1
+      else:
+        Mark("string too long")
+    ch = TextsRead(R)
+  str_[i] = 0x0; i+=1; ch = TextsRead(R); slen = i
+
+##  PROCEDURE HexString;
+##    VAR i, m, n: INTEGER;
+##  BEGIN i := 0; ch = TextsRead(R);
+##    WHILE ~R.eot & (ch # "$") DO
+##      WHILE (ch = " ") OR (ch = 9X) OR (ch = 0DX) DO ch = TextsRead(R) END ;  (*skip*)
+##      IF ("0" <= ch) & (ch <= "9") THEN m := ORD(ch) - 30H
+##      ELSIF ("A" <= ch) & (ch <= "F") THEN m := ORD(ch) - 37H
+##      ELSE m := 0; Mark("hexdig expected")
+##      END ;
+##      ch = TextsRead(R);
+##      IF ("0" <= ch) & (ch <= "9") THEN n := ORD(ch) - 30H
+##      ELSIF ("A" <= ch) & (ch <= "F") THEN n := ORD(ch) - 37H
+##      ELSE n := 0; Mark("hexdig expected")
+##      END ;
+##      IF i < stringBufSize THEN str[i] := CHR(m*10H + n); INC(i) ELSE Mark("string too long") END ;
+##      ch = TextsRead(R)
+##    END ;
+##    ch = TextsRead(R); slen := i  (*no 0X appended!*)
+##  END HexString;
+
+def HexString()
+#  VAR i, m, n: INTEGER;
+  i := 0; ch = TextsRead(R);
+  while not R_eot and (ch != "$"):
+    while (ch == " ") or (ch == 0x9) or (ch = 0DX):
+      ch = TextsRead(R)
+
+    if ("0" <= ch) and (ch <= "9"):
+      m = ord(ch) - 0x30
+    elif ("A" <= ch) and (ch <= "F"):
+      m = ord(ch) - 0x37
+    else:
+      m = 0; Mark("hexdig expected")
+
+    ch = TextsRead(R);
+
+    IF ("0" <= ch) & (ch <= "9") THEN n := ORD(ch) - 30H
+    ELSIF ("A" <= ch) & (ch <= "F") THEN n := ORD(ch) - 37H
+    ELSE n := 0; Mark("hexdig expected")
+    END ;
+
+    IF i < stringBufSize THEN str[i] := CHR(m*10H + n); INC(i) ELSE Mark("string too long") END ;
+
+    ch = TextsRead(R)
+
+  END ;
+  ch = TextsRead(R); slen := i  (*no 0X appended!*)
+END HexString;
+
+'''
   PROCEDURE Ten(e: LONGINT): REAL;
     VAR x, t: REAL;
   BEGIN x := 1.0; t := 10.0;
@@ -156,7 +276,7 @@ def Mark(msg):
   BEGIN ival := 0; i := 0; n := 0; k := 0;
     REPEAT
       IF n < 16 THEN d[n] := ORD(ch)-30H; INC(n) ELSE Mark("too many digits"); n := 0 END ;
-      Texts.Read(R, ch)
+      ch = TextsRead(R)
     UNTIL (ch < "0") OR (ch > "9") & (ch < "A") OR (ch > "F");
     IF (ch = "H") OR (ch = "R") OR (ch = "X") THEN  (*hex*)
       REPEAT h := d[i];
@@ -168,9 +288,9 @@ def Mark(msg):
       ELSIF ch = "R" THEN sym := real; rval := SYSTEM.VAL(REAL, k)
       ELSE sym := int; ival := k
       END ;
-      Texts.Read(R, ch)
+      ch = TextsRead(R)
     ELSIF ch = "." THEN
-      Texts.Read(R, ch);
+      ch = TextsRead(R);
       IF ch = "." THEN (*double dot*) ch := 7FX;  (*decimal integer*)
         REPEAT
           IF d[i] < 10 THEN
@@ -189,17 +309,17 @@ def Mark(msg):
         WHILE (ch >= "0") & (ch <= "9") DO (*fraction*)
           h := k*10 + ORD(ch) - 30H;
           IF h < maxM THEN k := h ELSE Mark("too many digits*") END ;
-          DEC(e); Texts.Read(R, ch)
+          DEC(e); ch = TextsRead(R)
         END ;
         x := FLT(k);
         IF (ch = "E") OR (ch = "D") THEN  (*scale factor*)
-          Texts.Read(R, ch); s := 0; 
-          IF ch = "-" THEN negE := TRUE; Texts.Read(R, ch)
+          ch = TextsRead(R); s := 0; 
+          IF ch = "-" THEN negE := TRUE; ch = TextsRead(R)
           ELSE negE := FALSE;
-            IF ch = "+" THEN Texts.Read(R, ch) END
+            IF ch = "+" THEN ch = TextsRead(R) END
           END ;
           IF (ch >= "0") & (ch <= "9") THEN
-            REPEAT s := s*10 + ORD(ch)-30H; Texts.Read(R, ch)
+            REPEAT s := s*10 + ORD(ch)-30H; ch = TextsRead(R)
             UNTIL (ch < "0") OR (ch >"9");
             IF negE THEN e := e-s ELSE e := e+s END
           ELSE Mark("digit?")
@@ -225,51 +345,51 @@ def Mark(msg):
   END Number;
 
   PROCEDURE comment;
-  BEGIN Texts.Read(R, ch);
+  BEGIN ch = TextsRead(R);
     REPEAT
       WHILE ~R.eot & (ch # "*") DO
-        IF ch = "(" THEN Texts.Read(R, ch);
+        IF ch = "(" THEN ch = TextsRead(R);
           IF ch = "*" THEN comment END
-        ELSE Texts.Read(R, ch)
+        ELSE ch = TextsRead(R)
         END
       END ;
-      WHILE ch = "*" DO Texts.Read(R, ch) END
+      WHILE ch = "*" DO ch = TextsRead(R) END
     UNTIL (ch = ")") OR R.eot;
-    IF ~R.eot THEN Texts.Read(R, ch) ELSE Mark("unterminated comment") END
+    IF ~R.eot THEN ch = TextsRead(R) ELSE Mark("unterminated comment") END
   END comment;
 
   PROCEDURE Get*(VAR sym: INTEGER);
   BEGIN
     REPEAT
-      WHILE ~R.eot & (ch <= " ") DO Texts.Read(R, ch) END;
+      WHILE ~R.eot & (ch <= " ") DO ch = TextsRead(R) END;
       IF ch < "A" THEN
         IF ch < "0" THEN
           IF ch = 22X THEN String; sym := string
-          ELSIF ch = "#" THEN Texts.Read(R, ch); sym := neq
+          ELSIF ch = "#" THEN ch = TextsRead(R); sym := neq
           ELSIF ch = "$" THEN HexString; sym := string
-          ELSIF ch = "&" THEN Texts.Read(R, ch); sym := and
-          ELSIF ch = "(" THEN Texts.Read(R, ch); 
+          ELSIF ch = "&" THEN ch = TextsRead(R); sym := and
+          ELSIF ch = "(" THEN ch = TextsRead(R); 
             IF ch = "*" THEN sym := null; comment ELSE sym := lparen END
-          ELSIF ch = ")" THEN Texts.Read(R, ch); sym := rparen
-          ELSIF ch = "*" THEN Texts.Read(R, ch); sym := times
-          ELSIF ch = "+" THEN Texts.Read(R, ch); sym := plus
-          ELSIF ch = "," THEN Texts.Read(R, ch); sym := comma
-          ELSIF ch = "-" THEN Texts.Read(R, ch); sym := minus
-          ELSIF ch = "." THEN Texts.Read(R, ch);
-            IF ch = "." THEN Texts.Read(R, ch); sym := upto ELSE sym := period END
-          ELSIF ch = "/" THEN Texts.Read(R, ch); sym := rdiv
-          ELSE Texts.Read(R, ch); (* ! % ' *) sym := null
+          ELSIF ch = ")" THEN ch = TextsRead(R); sym := rparen
+          ELSIF ch = "*" THEN ch = TextsRead(R); sym := times
+          ELSIF ch = "+" THEN ch = TextsRead(R); sym := plus
+          ELSIF ch = "," THEN ch = TextsRead(R); sym := comma
+          ELSIF ch = "-" THEN ch = TextsRead(R); sym := minus
+          ELSIF ch = "." THEN ch = TextsRead(R);
+            IF ch = "." THEN ch = TextsRead(R); sym := upto ELSE sym := period END
+          ELSIF ch = "/" THEN ch = TextsRead(R); sym := rdiv
+          ELSE ch = TextsRead(R); (* ! % ' *) sym := null
           END
         ELSIF ch < ":" THEN Number(sym)
-        ELSIF ch = ":" THEN Texts.Read(R, ch);
-          IF ch = "=" THEN Texts.Read(R, ch); sym := becomes ELSE sym := colon END 
-        ELSIF ch = ";" THEN Texts.Read(R, ch); sym := semicolon
-        ELSIF ch = "<" THEN  Texts.Read(R, ch);
-          IF ch = "=" THEN Texts.Read(R, ch); sym := leq ELSE sym := lss END
-        ELSIF ch = "=" THEN Texts.Read(R, ch); sym := eql
-        ELSIF ch = ">" THEN Texts.Read(R, ch);
-          IF ch = "=" THEN Texts.Read(R, ch); sym := geq ELSE sym := gtr END
-        ELSE (* ? @ *) Texts.Read(R, ch); sym := null
+        ELSIF ch = ":" THEN ch = TextsRead(R);
+          IF ch = "=" THEN ch = TextsRead(R); sym := becomes ELSE sym := colon END 
+        ELSIF ch = ";" THEN ch = TextsRead(R); sym := semicolon
+        ELSIF ch = "<" THEN  ch = TextsRead(R);
+          IF ch = "=" THEN ch = TextsRead(R); sym := leq ELSE sym := lss END
+        ELSIF ch = "=" THEN ch = TextsRead(R); sym := eql
+        ELSIF ch = ">" THEN ch = TextsRead(R);
+          IF ch = "=" THEN ch = TextsRead(R); sym := geq ELSE sym := gtr END
+        ELSE (* ? @ *) ch = TextsRead(R); sym := null
         END
       ELSIF ch < "[" THEN Identifier(sym)
       ELSIF ch < "a" THEN
@@ -278,7 +398,7 @@ def Mark(msg):
         ELSIF ch = "^" THEN sym := arrow
         ELSE (* _ ` *) sym := null
         END ;
-        Texts.Read(R, ch)
+        ch = TextsRead(R)
       ELSIF ch < "{" THEN Identifier(sym) ELSE
         IF ch = "{" THEN sym := lbrace
         ELSIF ch = "}" THEN sym := rbrace
@@ -287,59 +407,17 @@ def Mark(msg):
         ELSIF ch = 7FX THEN  sym := upto
         ELSE sym := null
         END ;
-        Texts.Read(R, ch)
+        ch = TextsRead(R)
       END
     UNTIL sym # null
   END Get;
 
   PROCEDURE Init*(T: Texts.Text; pos: LONGINT);
-  BEGIN errpos := pos; errcnt := 0; Texts.OpenReader(R, T, pos); Texts.Read(R, ch)
+  BEGIN errpos := pos; errcnt := 0; Texts.OpenReader(R, T, pos); ch = TextsRead(R)
   END Init;
 
   PROCEDURE EnterKW(sym: INTEGER; name: ARRAY OF CHAR);
   BEGIN keyTab[k].id := name; keyTab[k].sym := sym; INC(k)
   END EnterKW;
 
-BEGIN Texts.OpenWriter(W); k := 0; KWX[0] := 0; KWX[1] := 0;
-  EnterKW(if, "IF");
-  EnterKW(do, "DO");
-  EnterKW(of, "OF");
-  EnterKW(or, "OR");
-  EnterKW(to, "TO");
-  EnterKW(in, "IN");
-  EnterKW(is, "IS");
-  EnterKW(by, "BY");
-  KWX[2] := k;
-  EnterKW(end, "END");
-  EnterKW(nil, "NIL");
-  EnterKW(var, "VAR");
-  EnterKW(div, "DIV");
-  EnterKW(mod, "MOD");
-  EnterKW(for, "FOR");
-  KWX[3] := k;
-  EnterKW(else, "ELSE");
-  EnterKW(then, "THEN");
-  EnterKW(true, "TRUE");
-  EnterKW(type, "TYPE");
-  EnterKW(case, "CASE");
-  KWX[4] := k;
-  EnterKW(elsif, "ELSIF");
-  EnterKW(false, "FALSE");
-  EnterKW(array, "ARRAY");
-  EnterKW(begin, "BEGIN");
-  EnterKW(const, "CONST");
-  EnterKW(until, "UNTIL");
-  EnterKW(while, "WHILE");
-  KWX[5] := k;
-  EnterKW(record, "RECORD");
-  EnterKW(repeat, "REPEAT");
-  EnterKW(return, "RETURN");
-  EnterKW(import, "IMPORT");
-  EnterKW(module, "MODULE");
-  KWX[6] := k;
-  EnterKW(pointer, "POINTER");
-  KWX[7] := k; KWX[8] := k;
-  EnterKW(procedure, "PROCEDURE");
-  KWX[9] := k
-END ORS.
 '''
