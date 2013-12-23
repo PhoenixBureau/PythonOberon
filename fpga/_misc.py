e53aba916444de71e4a1f66fccf0b7ef6f5376e6
Simon Forman <forman.simon@gmail.com>
2013-12-22 21:06:52 -0800

    A crude assembler, incomplete.
    I just need to emit some binary instructions to play.

---
+++ fpga/_misc.py
@@ -0,0 +1,53 @@
+from myhdl import intbv, concat
+
+ops = dict(
+  Mov = 0, Lsl = 1, Asr = 2, Ror= 3, And = 4, Ann = 5, Ior = 6, Xor = 7,
+  Add = 8, Sub = 9, Mul = 10, Div = 11,
+  )
+word = lambda n: intbv(n, min=0, max=2**32)
+
+
+
+def make_F0(u, op, a, b, c):
+  assert bool(u) == u, repr(u)
+  assert ops['Mov'] <= op <= ops['Div'], repr(op)
+  assert 0 <= a < 0x10, repr(a)
+  assert 0 <= b < 0x10, repr(b)
+  assert 0 <= c < 0x10, repr(c)
+  return word(
+    (u << 29) +
+    (a << 24) +
+    (b << 20) +
+    (op << 16) +
+    c
+    )
+
+
+for op in sorted(ops, key=ops.get):
+  print '''def %s(a, b, c, u=0): return make_F0(u, %i, a, b, c)''' % (op, ops[op])
+
+print ; print
+
+for op in sorted(ops, key=ops.get):
+  print '''def %s_imm(a, b, K, v=0, u=0): return make_F1(u, v, %i, a, b, K)''' % (op, ops[op])
+
+
+##def control_unit():
+##  @always(clk.posedge)
+##  def next_PC():
+##    IR.next = codebus.val
+##    if not rst:
+##      pcmux = 0
+##    elif stall:
+##      pcmux = PC.val
+##    elif IR[32:28] == 14 and condition(): # BR
+##        if u:
+##          pcmux = PC.val - imm.val # wrong
+##        else:
+##          pcmux = C0[19:2]
+##    else:
+##      pcmux = PC + 1
+##    PC.next = pcmux
+##  return next_PC
+
+
