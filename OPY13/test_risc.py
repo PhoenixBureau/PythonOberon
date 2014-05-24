6b1b513762d183f604a1ab7f84bc52caf7119e8c
Simon Forman <sforman@hushmail.com>
2014-05-24 11:41:49 -0700

    Tests for the "ram".

--- OPY13/test_risc.py
+++ OPY13/test_risc.py
@@ -21,6 +21,26 @@ class TestSequenceFunctions(unittest.TestCase):
     self.ram[24] = 18
     self.assertEqual(self.ram[24], 18)
 
+  def test_put_word_get_byte(self):
+    n = 24
+    self.ram[n] = 0xdeadbeef
+    for i, byte in zip(
+      reversed(range(n, n + 4)),
+      (0xde, 0xad, 0xbe, 0xef),
+      ):
+      mem = self.ram.get_byte(i)
+      self.assertEqual(byte, mem)
+
+  def test_put_byte_get_word(self):
+    n = 24
+    for i, byte in zip(
+      reversed(range(n, n + 4)),
+      (0xde, 0xad, 0xbe, 0xef),
+      ):
+      self.ram.put_byte(i, byte)
+    word = self.ram[n]
+    self.assertEqual(0xdeadbeef, word)
+
 
 if __name__ == '__main__':
     unittest.main()
