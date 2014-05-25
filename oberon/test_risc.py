a5359b04625638f37a15dec2b663048e1d172bf9
Simon Forman <simon@mydream.com>
2014-05-25 02:02:32 +0000

    test_Mov_imm

--- oberon/test_risc.py
+++ oberon/test_risc.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, traceback
 from mock import MagicMock
 from risc import RISC, ByteAddressed32BitRAM
 
@@ -9,15 +9,31 @@ class TestRISC(unittest.TestCase):
     self.ram = MagicMock(name='RAM')
     self.cpu = RISC(self.ram)
 
-  def test_what(self):
-    try:
-      self.cpu.cycle()
-    except:
-      pass
-    print self.ram.mock_calls
+  def test_Mov_imm(self):
+    # Mov_imm(8, 1)
+    self.ram.__getitem__.return_value = 0b1001000000000000000000000000001
+    self.cpu.cycle()
+    self.assertEqual(1, self.cpu.R[8])
 
-##    self.ram.get.return_value = 0
-##    self.assertEqual(self.ram.get(24), 0)
+##  def test_what(self):
+##    self.ram.__getitem__.return_value = 0
+##    print
+##
+#    self.cpu.view() #  FIXME:
+##    Traceback (most recent call last):
+##      File "test_risc.py", line 15, in test_what
+##        self.cpu.view()
+##      File "risc.py", line 187, in view
+##        kw['A'] = self.R[self.ira]
+##    AttributeError: 'RISC' object has no attribute 'ira'
+##
+##    try:
+##      self.cpu.cycle()
+##    except:
+##      traceback.print_exc()
+##    self.cpu.view()
+##    print self.ram.mock_calls
+##    print '-' * 40
 
 
 class TestByteAddressed32BitRAM(unittest.TestCase):
