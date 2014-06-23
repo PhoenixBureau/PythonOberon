3f084f3a9b98d2a2a61c1b84cc69909b17566f99
Simon Forman <sforman@hushmail.com>
2014-06-22 22:29:16 -0700

    Handle negative numbers a little better.

--- oberon/risc.py
+++ oberon/risc.py
@@ -1,7 +1,8 @@
 from pprint import pformat
-from myhdl import intbv, concat
+from myhdl import intbv
 from util import ibv, bits2signed_int, signed
 from assembler import dis
+from signs import py2signed, signed2py
 
 
 F = 2**32-1
@@ -19,7 +20,7 @@ class RISC(object):
   def __init__(self, ram, PC=0):
     self.ram = ram
     self.PC = self.pcnext = PC
-    self.R = [0 for i in range(16)]
+    self.R = [0] * 16
     self.H = 0
     self.N = self.Z = self.C = self.OV = 0
     self.io_ports = {}
@@ -32,20 +33,20 @@ class RISC(object):
     self.control_unit()
 
   def decode(self, instruction):
-    self.IR = IR = ibv(32, instruction)
-    self.p = IR(31)
-    self.q = IR(30)
-    self.u = IR(29)
-    self.v = IR(28)
-    self.w = IR(16)
-    self.op = IR(20, 16)
-    self.ira = IR(28, 24)
-    self.irb = IR(24, 20)
-    self.irc = IR(4, 0)
-    self.cc = IR(27, 24)
-    self.imm = IR(16, 0)
-    self.off = IR(20, 0)
-    self.jmp = IR(24, 0)
+    self.IR = IR = bint(instruction)
+    self.p = IR[31]
+    self.q = IR[30]
+    self.u = IR[29]
+    self.v = IR[28]
+    self.w = IR[16]
+    self.op = IR[20, 16]
+    self.ira = IR[28, 24]
+    self.irb = IR[24, 20]
+    self.irc = IR[4, 0]
+    self.cc = IR[27, 24]
+    self.imm = IR[16, 0]
+    self.off = IR[20, 0]
+    self.jmp = IR[24, 0]
     self.C0 = self.R[self.irc]
 
   def what_are_we_up_to(self):
@@ -79,12 +80,39 @@ class RISC(object):
   def Arithmetic_Logical_Unit(self):
     B = self.R[self.irb]
 
-    C1 = self.C1 = concat(
-      self.v, self.v, self.v, self.v,
-      self.v, self.v, self.v, self.v,
-      self.v, self.v, self.v, self.v,
-      self.v, self.v, self.v, self.v,
-      self.imm,
+    # Here's how negative immediate values are stored in the instruction and
+    # regenerated in the cpu. In the ORGX module the instruction is created
+    # like so:
+    #
+    #  PROCEDURE Put1(op, a, b, im: LONGINT);
+    #  BEGIN (*emit format-1 instruction,  -10000H <= im < 10000H*)
+    #    IF im < 0 THEN INC(op, 1000H) END ;  (*set v-bit*)
+    #    code[pc] := (((a+40H) * 10H + b) * 10H + op) * 10000H + (im MOD 10000H); INC(pc)
+    #  END Put1;
+    #
+    # If the immediate value is negative the V bit in the instruction is set.
+    # In any event the immediate value has it's high sixteen bits masked off
+    # (modulus 0x10000 effects this.)  For example, -23 looks like this,
+    # bit-wise:
+    #
+    #   11111111111111111111111111101001
+    #
+    # And after truncating the high bits:
+    #
+    #                   1111111111101001
+    #
+    # This is the bit pattern that gets stored in the instruction immediate
+    # field.
+    #
+    # The statement immediately below reconstructs the negative 32-bit value
+    # if the V bit is set in the instruction, otherwise it simply passes
+    # through the given immediate value. (This happens, of coures, only if
+    # the Q bit is set, otherwise C1 is just set to C0.)
+
+    C1 = self.C1 = (
+      (0b11111111111111110000000000000000 | self.imm)
+      if self.v else
+      self.imm
       ) if self.q else self.C0
 
     if self.MOV:
@@ -100,7 +128,16 @@ class RISC(object):
           if not self.irc[0]:
             res = self.H
           else:
-            res = concat(self.N, self.Z, self.C, self.OV) << 28 | 80
+            res = (
+              self.N << 31 |
+              self.Z << 30 |
+              self.C << 29 |
+              self.OV << 28 |
+              80
+              )
+
+    # Bit-wise logical operations
+
     elif self.LSL:
       res = B << C1
     elif self.ASR or self.ROR:
@@ -108,35 +145,62 @@ class RISC(object):
     elif self.AND:
       res = B & C1
     elif self.ANN:
-      res = B & ((2**32-1) ^ C1)
+      res = B & (F ^ C1)
     elif self.IOR:
       res = B | C1
     elif self.XOR:
       res = B ^ C1
+
+    # For the arithmetical operators we must convert to Python ints to
+    # correctly handle negative numbers.
+
     elif self.ADD:
-      x = B + C1 + (self.u and self.C)
-      res = signed(x, 32)
+      B = signed2py(B)
+      C1 = signed2py(C1)
+      res = B + C1 + (self.u and self.C)
+      res = self._check_overflow(res)
+
     elif self.SUB:
+      B = signed2py(B)
+      C1 = signed2py(C1)
       res = B - C1 - (self.u and self.C)
-      res = signed(res, 32)
+      res = self._check_overflow(res)
+
     elif self.MUL:
-      self.product = intbv(B * C1)
+      B = signed2py(B)
+      C1 = signed2py(C1)
+      product = B * C1
+      self.product = blong(py2signed(product, 64))
       res = self.product[32:0]
+
     elif self.DIV:
-      res = intbv(B / C1)
-      self.remainder = B % C1
-##      res, self.remainder = divmod(B, C1)
+      B = signed2py(B)
+      C1 = signed2py(C1)
+      res, remainder = divmod(B, C1)
+      res = py2signed(res)
+      self.remainder = py2signed(remainder)
+
     else:
       res = 0
 
-    return res if isinstance(res, intbv) else intbv(res)[32:0]
+    return res if isinstance(res, bint) else bint(res)
+
+  def _check_overflow(self, res, bits=33):
+    try:
+      return py2signed(res)
+    except ValueError:
+      self.OV = True
+      return blong(py2signed(res, bits))[32:0]
+    self.OV = False
 
   def register_instruction(self):
     self.pcnext = self.PC + 1
-    self.R[self.ira] = regmux = self.Arithmetic_Logical_Unit()
+    regmux = self.Arithmetic_Logical_Unit()
+    self.R[self.ira] = regmux[32:0]
     self.N = regmux[31]
-    self.Z = (regmux[31:0] == 0)
-    self.C = regmux[32] if (self.ADD|self.SUB) else self.C
+    self.Z = regmux == 0
+    if self.ADD | self.SUB:
+      self.C = regmux[32]
 #    self.OV = ... if (ADD|SUB) else OV
     self.H = (self.product[64:32] if self.MUL
               else self.remainder if self.DIV
@@ -160,7 +224,7 @@ class RISC(object):
       return
 
     if self.v: # Save link
-      self.R[15] = self.PC + 4
+      self.R[15] = self.PC + 1
 
     if self.u:
       self.pcnext = int(self.jmp + self.PC + 1)
@@ -238,18 +302,27 @@ class ByteAddressed32BitRAM(object):
 
   def put_byte(self, addr, byte):
     if isinstance(byte, str):
-      assert len(byte) == 1, repr(byte)
-      byte = ord(byte)
-    assert 0 <= byte < 256, repr(byte)
+      byte = ord(byte[:1])
+    if not (0 <= byte < 256):
+      raise ValueError("byte out of range: %i" % (byte,))
+
     word_addr, byte_offset = divmod(addr, 4)
-    n = 8 * byte_offset
+    n = 8 * byte_offset # How many bits to shift.
     byte <<= n
-    try:
+
+    try: # Get the current memory contents, if any.
       word = self.store[word_addr]
-    except KeyError:
-      pass # just store shifted byte or
-    else: # merge word and shifted byte.
+    except KeyError: # nothing there yet so
+      pass # just store shifted byte, or
+    else: # merge word and shifted byte
       mask = F ^ (255 << n)
+      # mask should now be one of:
+      # 0b11111111111111111111111100000000
+      # 0b11111111111111110000000011111111
+      # 0b11111111000000001111111111111111
+      # 0b00000000111111111111111111111111
+      # Now AND the mask with the memory word to clear the bits for the
+      # pre-shifted byte, and OR the result with same.
       byte |= word & mask
     self.store[word_addr] = byte
 
@@ -258,6 +331,37 @@ class ByteAddressed32BitRAM(object):
 
   def __repr__(self):
     return pformat(self.store)
+
+
+class binary_addressing_mixin(object):
+
+  def __getitem__(self, n):
+    if isinstance(n, tuple):
+      if len(n) != 2:
+        raise IndexError('Must pass only two indicies.')
+      start, stop = n
+      return self._mask(stop, start - stop)
+    if isinstance(n, slice):
+      return self._getslice(n)
+    return bool(self >> n & 1)
+
+  def _getslice(self, s):
+    n = s.start - s.stop
+    if n < 0:
+      raise IndexError('Slice indexes should be left-to-right.')
+    if not n:
+      raise IndexError('Zero bits.')
+    if s.step:
+      raise TypeError('Slice with step not supported.')
+
+    return self._mask(s.stop, n)
+
+  def _mask(self, stop, n):
+    return type(self)(self >> stop & (2**n - 1))
+
+
+class bint(binary_addressing_mixin, int): pass
+class blong(binary_addressing_mixin, long): pass
 
 
 if __name__ == '__main__':
