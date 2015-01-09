ef55f01d229491738b27d9ffd91f28421add1360
Simon Forman <sforman@hushmail.com>
2015-01-09 11:12:48 -0800

    Remove all the old cruft.

--- exercise_ram.py
+++
@@ -1,8 +0,0 @@
-from risc import ByteAddressed32BitRAM
-
-
-d = ByteAddressed32BitRAM()
-for n, ch in enumerate("Hello world!"):
-  d.put_byte(n, ch)
-print d
-print ''.join(chr(d.get_byte(n)) for n in range(12))
