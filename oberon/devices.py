a8d51db09f5cc3befc91a2847d2486b923633a99
Simon Forman <sforman@hushmail.com>
2015-01-09 10:53:21 -0800

    Replace DMA disk with one that works.
    The new disk class is pretty much a direct port of the one in
    Peter De Wachter's C emulator.  It has the advantage of actually working.

--- oberon/devices.py
+++ oberon/devices.py
@@ -1,84 +1,131 @@
 from sys import stderr
 from time import time
 from collections import defaultdict
+import pdb
+from struct import unpack
 
 
-dmiRead = 0
-dmiWrite = 1
-SectorLength = 1024
+diskCommand, diskRead, diskWrite, diskWriting = 0, 1, 2, 3
 
 
-class BlockDeviceWithDMI(object):
+def log(message, *args):
+  print message % args
+##  print >> stderr, message % args
 
-  READ_COMMAND = 0
-  READ_SRC = 1
-  READ_DST = 2
 
-  def __init__(self, ram, data=None):
-    self.ram = ram
-    if data is None:
-      data = {}
-    self.data = data
-    self.state = self.READ_COMMAND
-    self.direction = dmiRead
-    self.src = self.dst = 0
+class Disk(object):
 
-  def write(self, word):
-    if self.state == self.READ_COMMAND:
-      assert word in {dmiRead, dmiWrite}, repr(word)
-      self.direction = word
-      self.state = self.READ_SRC
-      print >> stderr, 'disk', ('dmiRead', 'dmiWrite')[word]
+  SECTOR_SIZE = 512
+  SECTOR_SIZE_WORDS = SECTOR_SIZE / 4
 
-    elif self.state == self.READ_SRC:
-      self.src = word
-      self.state = self.READ_DST
-      print >> stderr, 'disk src <-', hex(word)
+  def __init__(self, filename='002RISC.img'):
+    self.state = diskCommand
 
-    else:
-      assert self.state == self.READ_DST
-      self.dst = word
-      self.state = self.READ_COMMAND
-      print >> stderr, 'disk dst <-', hex(word)
+    self.rx_buf = [None] * self.SECTOR_SIZE_WORDS
+    self.rx_idx = 0
 
-      if self.direction == dmiRead:
-        self.read_block()
-      else:
-        self.write_block()
+    self.tx_buf = [None] * (self.SECTOR_SIZE_WORDS + 2)
+    self.tx_cnt = 0
+    self.tx_idx = 0
 
-  def read_block(self):
-    sector = self.data[self.src]
-    ram_addrs = range(self.dst, self.dst + SectorLength, 4)
-    print >> stderr, 'disk read_block RAM: 0x%x <- disk: 0x%x' % (self.dst, self.src)
-    for addr, word in zip(ram_addrs, sector):
-      self.ram[addr] = word
-
-  def write_block(self):
-    sector = map(
-      self._ram_or_zero,
-      xrange(self.src, self.src + SectorLength, 4)
-      )
-    self.data[self.dst] = sector
-    print >> stderr, 'disk write_block disk: 0x%x <- RAM: 0x%x' % (self.dst, self.src)
-
-  def _ram_or_zero(self, addr):
-    try:
-      return self.ram[addr]
-    except KeyError:
-      return 0
+    self.file = open(filename, 'rb')
+    self.read_sector()
+    self.offset = 0x80002 if self.tx_buf[0] == 0x9B1EA38D else 0
 
   def read(self):
-    print >> stderr, 'disk read() RAM: 0x%x <- disk: 0x%x' % (self.dst, self.src)
-    return 1
-    raise NotImplementedError
+    if self.tx_idx >= 0 and self.tx_idx < self.tx_cnt:
+      log('disk_read from buffer 0x%x', self.tx_buf[self.tx_idx])
+      return self.tx_buf[self.tx_idx]
+    log('disk_read from default 0xFF')
+    return 255
 
+  def write(self, word):
+    log('disk_write 0x%x', word)
 
-def fake_sector():
-  return [0xdeadbeef for n in xrange(0, SectorLength, 4)]
+    self.tx_idx += 1
 
+    if self.state == diskCommand:
+      if (0xff & word) == 0xff and self.rx_idx == 0:
+        log('disk_write PASS 0x%x', word)
+        return
+      log('disk_write diskCommand 0x%x to rx_buf[%i]', word, self.rx_idx)
+      self.rx_buf[self.rx_idx] = word
+      self.rx_idx += 1
+      if self.rx_idx == 6:
+##        pdb.set_trace()
+        self.run_command()
+        self.rx_idx = 0
 
-def fake_disk():
-  return defaultdict(fake_sector)
+    elif self.state == diskRead:
+      if self.tx_idx == self.tx_cnt:
+        self.state = diskCommand
+        log('disk_write diskRead -> diskCommand')
+        self.tx_cnt = 0
+        self.tx_idx = 0
+
+    elif self.state == diskWrite:
+      if word == 254:
+        self.state = diskWriting
+        log('disk_write diskWrite -> diskWriting')
+
+    elif self.state == diskWriting:
+      if self.rx_idx < 128:
+        self.rx_buf[self.rx_idx] = word
+
+      self.rx_idx += 1
+
+      if self.rx_idx == 128:
+        self.write_sector()
+
+      if self.rx_idx == 130:
+        self.tx_buf[0] = 5
+        self.tx_cnt = 1
+        self.tx_idx = -1
+        self.rx_idx = 0
+        self.state = diskCommand
+        log('disk_write diskWriting -> diskCommand')
+
+  def run_command(self):
+    cmd, a, b, c, d = self.rx_buf[0:5]
+    a, b, c, d = (n & 0xff for n in (a, b, c, d))
+    arg = (a << 24) | (b << 16) | (c << 8) | d
+    log('run_command ' + ' '.join(map(hex, (cmd, arg))))
+
+    if cmd == 81:
+      self.state = diskRead
+      self.tx_buf[0] = 0
+      self.tx_buf[1] = 254
+      self._seek(arg)
+##      pdb.set_trace()
+      self.read_sector(2)
+      self.tx_cnt = 2 + 128
+
+    elif cmd == 88:
+      self.state = diskWrite
+      self._seek(arg)
+      self.tx_buf[0] = 0
+      self.tx_cnt = 1
+
+    else:
+      self.tx_buf[0] = 0
+      self.tx_cnt = 1
+
+    self.tx_idx = -1
+
+  def _seek(self, arg):
+    log('#' * 100)
+    a = (arg - self.offset) * self.SECTOR_SIZE
+    log('seeking to %i (0x%x)', arg, a)
+    self.file.seek(a)
+      
+  def read_sector(self, into=0):
+    data = self.file.read(self.SECTOR_SIZE)
+    self.tx_buf[into:] = unpack('<128I', data)
+
+  def write_sector(self):
+    log('write sector %r', self.rx_buf)
+#    data = pack('<128I', self.rx_buf)
+#    self.file.write(data)
 
 
 class clock(object):
@@ -120,14 +167,5 @@ class FakeSPI(object):
 
 
 if __name__ == '__main__':
-  from pprint import pprint
-  from ram import ByteAddressed32BitRAM
-  d = ByteAddressed32BitRAM()
-  for n, ch in enumerate("Hello world!"):
-    d.put_byte(n, ord(ch))
-  b = BlockDeviceWithDMI(d)
-  pprint(b.data)
-  b.write(dmiWrite)
-  b.write(0)
-  b.write(0)
-  pprint(b.data)
+  disk = Disk()
+  print disk.read_data()
