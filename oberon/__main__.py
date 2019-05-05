f95496ef45c9d7030b8859326ead1eb323fed696
Simon Forman <sforman@hushmail.com>
2019-05-04 23:36:40 -0700

    Cleanup __main__ module to use demo.

--- oberon/__main__.py
+++ oberon/__main__.py
@@ -1,41 +1,18 @@
 from sys import stderr
 from traceback import print_exc
-from .risc import (
-  RISC,
-  Disk,
-  clock,
-  LEDs,
-  FakeSPI,
-  Mouse,
-  ByteAddressed32BitRAM,
-  MemWords,
-  )
 from .bootloader import bootloader
-from .display import initialize_screen, ScreenRAMMixin
-
-
-class Memory(ScreenRAMMixin, ByteAddressed32BitRAM):
-  pass
+from .demo import Memory, make_cpu
+from .display import initialize_screen
+from .risc import Disk
+import pygame
 
 
 memory = Memory()
 memory.set_screen(initialize_screen())
-
 disk = Disk('disk.img')
-
-risc_cpu = RISC(bootloader, memory)
-
-risc_cpu.io_ports[0] = clock()
-risc_cpu.io_ports[4] = LEDs()
-#  risc_cpu.io_ports[8] = RS232 data
-#  risc_cpu.io_ports[12] = RS232 status
-risc_cpu.io_ports[20] = fakespi = FakeSPI()
-risc_cpu.io_ports[16] = fakespi.data
-risc_cpu.io_ports[24] = mouse = Mouse()
-
+cpu = make_cpu(bootloader, memory, disk)
+mouse = cpu.io_ports[24]
 mouse.set_coords(450, 474) # Imitate values in C trace.
-
-fakespi.register(1, disk)
 
 
 def cycle(cpu, limit):
@@ -48,10 +25,11 @@ def cycle(cpu, limit):
       cpu.dump_ram()
       break
 
-    if cpu.PC < MemWords:
-      if not n % 10000:
-        print >> stderr, n
-      n += 1
+    if not n % 10000:
+      pygame.event.pump()
+      print >> stderr, n
+    n += 1
 
 
-cycle(risc_cpu, 8000000)
+# Details begin to be painted around 6.5M cycles.
+cycle(cpu, 8000000)
