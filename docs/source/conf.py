e383e330f303ec031f526fccfbca2b5442ddaa64
Simon Forman <sforman@hushmail.com>
2020-12-11 09:33:32 -0800

    Use Py3 on ReadTheDocs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
-project = u'PythonOberon'
+project = u'Python Oberon'
 copyright = u'2019, Simon Forman'
 author = u'Simon Forman'
 
